cba7e29b9075a4849a31d818618feccac070acc8
Novum Atmosphere <novum.atm@gmail.com>
2021-09-20 08:14:10 -0500

    Heroku 1.0.0

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const express = require('express');
 // Crear servidor express
 const app = express();
 
+app.use(express.static(__dirname+'/dist/angular-historia-clinica'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/angular-historia-clinica/login'));
+});
+
 // Puerto de escucha
-app.listen( 3001, () => {
-    console.log('Servidor corriendo en el puerto ' + 3001);
+app.listen( 8080, () => {
+    console.log('Servidor corriendo en el puerto ' + 8080);
 });
